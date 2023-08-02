93a1489c233d6be0dee4e97abaea5a232512c3c5
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-08-02 17:42:55 +0300

    Update displayMeal.js

--- src/modules/displayMeal.js
+++ src/modules/displayMeal.js
@@ -4,14 +4,14 @@ export default (meals) => {
 
   // Loop through each meal and create a list item with image and heart icon for it
   meals.slice(0, 30).forEach((meal, index) => {
-    const item_id = `item${index + 1}`; // Generate item_id dynamically starting from "item1"
+    const itemId = `item${index + 1}`; // Generate item_id dynamically starting from "item1"
     const listItem = `
       <li class="foodCard">
         <img class="mealimage" src="${meal.strMealThumb}" alt="${meal.strMeal}">
         <h2>${meal.strMeal}</h2>
         <i class="fa-regular fa-heart heart" style="color: #dd0808;"></i>
         <button type="button" class="comment">Comments</button>
-        <span class="likes" id="${item_id}_likes">0 Likes</span>
+        <span class="likes" id="${itemId}_likes">0 Likes</span>
       </li>
     `;
     listItems.push(listItem);
@@ -31,22 +31,22 @@ export default (meals) => {
 
 async function handleLike(event) {
   // Get the item_id from the meal name
-  const item_id = event.target.parentElement.querySelector('h2').textContent;
+  const itemId = event.target.parentElement.querySelector('h2').textContent;
 
   try {
     // Fetch the app_id from the API
     const appResponse = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps', {
       method: 'POST',
     });
-    const app_id = await appResponse.text(); // Receive the app_id as plain text
+    const appId = await appResponse.text(); // Receive the app_id as plain text
 
     // Send a POST request to create a new like for the item
-    const likeResponse = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${app_id}/likes`, {
+    const likeResponse = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appId}/likes`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ item_id }), // Convert the item_id to a JSON string
+      body: JSON.stringify({ itemId }), // Convert the item_id to a JSON string
     });
 
     /* eslint-disable no-plusplus */
