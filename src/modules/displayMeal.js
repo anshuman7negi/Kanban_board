038081b634919c27f21626ca1051fb744f9101bd
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-08-02 09:19:28 +0300

    Update displayMeal.js

--- src/modules/displayMeal.js
+++ src/modules/displayMeal.js
@@ -8,6 +8,7 @@ export default (meals) => {
          <li class="foodCard">
            <img class="mealimage" src="${meal.strMealThumb}" alt="${meal.strMeal}">
            <h2>${meal.strMeal}</h2>
+           <i class="fa-regular fa-heart heart" style="color: #dd0808;"></i>
            <button type="button" class="comment">Comments</button>
          </li>
        `;
