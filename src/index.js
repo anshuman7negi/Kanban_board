da3371efa313a45ceec9ce2c3ab94bab169a83da
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-08-02 09:37:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import './style.css';
-import { fetchAndDisplayMeals } from './modules/mealApi.js';
+import fetchAndDisplayMeals from './modules/mealApi.js';
 import displayMeal from './modules/displayMeal.js';
 import popupCard from './modules/popupCard.js';
 
 const displayData = async () => {
     const meals = await fetchAndDisplayMeals();
     displayMeal(meals);
-}
+};
 
 document.addEventListener('DOMContentLoaded', async () => {
     await displayData();
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     const comment = document.querySelectorAll('.comment');
     for (let i = 0; i < comment.length; i += 1) {
         comment[i].addEventListener('click', () => {
-            document.querySelector('main').style.display = "none";
+            document.querySelector('main').style.display = 'none';
             popupCard(i, meals);
         });
     }
@@ -22,9 +22,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 
 document.addEventListener('click', (event) => {
     if (event.target.classList.contains('close')) {
-        document.querySelector('main').style.display = "block";
-        document.querySelector('.popup').style.display = "none";
+        document.querySelector('main').style.display = 'block';
+        document.querySelector('.popup').style.display = 'none';
     }
 });
-
-
