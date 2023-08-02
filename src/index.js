ea0adc9520263f8f7e679044dc4a2746d754aa9e
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-08-02 09:49:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,25 +4,25 @@ import displayMeal from './modules/displayMeal.js';
 import popupCard from './modules/popupCard.js';
 
 const displayData = async () => {
-    const meals = await fetchAndDisplayMeals();
-    displayMeal(meals);
+  const meals = await fetchAndDisplayMeals();
+  displayMeal(meals);
 };
 
 document.addEventListener('DOMContentLoaded', async () => {
-    await displayData();
-    const meals = await fetchAndDisplayMeals();
-    const comment = document.querySelectorAll('.comment');
-    for (let i = 0; i < comment.length; i += 1) {
-        comment[i].addEventListener('click', () => {
-            document.querySelector('main').style.display = 'none';
-            popupCard(i, meals);
-        });
-    }
+  await displayData();
+  const meals = await fetchAndDisplayMeals();
+  const comment = document.querySelectorAll('.comment');
+  for (let i = 0; i < comment.length; i += 1) {
+    comment[i].addEventListener('click', () => {
+      document.querySelector('main').style.display = 'none';
+      popupCard(i, meals);
+    });
+  }
 });
 
 document.addEventListener('click', (event) => {
-    if (event.target.classList.contains('close')) {
-        document.querySelector('main').style.display = 'block';
-        document.querySelector('.popup').style.display = 'none';
-    }
+  if (event.target.classList.contains('close')) {
+    document.querySelector('main').style.display = 'block';
+    document.querySelector('.popup').style.display = 'none';
+  }
 });
