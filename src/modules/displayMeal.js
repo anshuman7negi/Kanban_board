b03a87cd97a2fe31a60803b95edff19aba2fd4b1
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-08-02 09:37:14 +0300

    Update displayMeal.js

--- src/modules/displayMeal.js
+++ src/modules/displayMeal.js
@@ -1,21 +1,21 @@
 export default (meals) => {
-     // Create an empty list to hold the HTML list items
-     const listItems = [];
-  
-     // Loop through each meal and create a list item with image for it
-     meals.slice(0, 10).forEach((meal) => {
-       const listItem = `
-         <li class="foodCard">
-           <img class="mealimage" src="${meal.strMealThumb}" alt="${meal.strMeal}">
-           <h2>${meal.strMeal}</h2>
-           <i class="fa-regular fa-heart heart" style="color: #dd0808;"></i>
-           <button type="button" class="comment">Comments</button>
-         </li>
-       `;
-       listItems.push(listItem);
-     });
+  // Create an empty list to hold the HTML list items
+  const listItems = [];
 
-     // Display the list on the webpage
-     const container = document.querySelector('#mealListContainer');
-     container.innerHTML = `<ul class="grid">${listItems.join('')}</ul>`;
-}
+  // Loop through each meal and create a list item with an image for it
+  meals.slice(0, 10).forEach((meal) => {
+      const listItem = `
+          <li class="foodCard">
+              <img class="mealimage" src="${meal.strMealThumb}" alt="${meal.strMeal}">
+              <h2>${meal.strMeal}</h2>
+              <i class="fa-regular fa-heart heart" style="color: #dd0808;"></i>
+              <button type="button" class="comment">Comments</button>
+          </li>
+      `;
+      listItems.push(listItem);
+  });
+
+  // Display the list on the webpage
+  const container = document.querySelector('#mealListContainer');
+  container.innerHTML = `<ul class="grid">${listItems.join('')}</ul>`;
+};
