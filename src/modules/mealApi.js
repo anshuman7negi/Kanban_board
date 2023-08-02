aaa151a884cfe97140279aa92b2f7de906b1fefa
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-08-02 09:37:22 +0300

    Update mealApi.js

--- src/modules/mealApi.js
+++ src/modules/mealApi.js
@@ -1,11 +1,16 @@
 // Function to fetch 10 meals from the API and display them in the list with images
-  const fetchAndDisplayMeals = async () => {
+const fetchAndDisplayMeals = async () => {
+  try {
       // Fetch data from the API for 10 meals
       const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
       const data = await response.json();
       const meals = data.meals;
       return meals;
+  } catch (error) {
+      console.error('Error fetching meals:', error);
+      return []; // Return an empty array in case of an error
   }
-  
-  export { fetchAndDisplayMeals};
-  
+};
+
+export default fetchAndDisplayMeals;
+ 
