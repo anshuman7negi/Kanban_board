c90f85343f42f893a0ac68a60fc0f8e68fd8700f
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-08-03 22:42:58 +0300

    Update itemsCounter.test.js

--- src/testing/itemsCounter.test.js
+++ src/testing/itemsCounter.test.js
@@ -1,43 +1,24 @@
-/* eslint-disable import/no-extraneous-dependencies */
 // Import the function to test
-import { JSDOM } from 'jsdom';
 import itemsCounter from '../modules/itemsCounter.js';
 
-const dom = new JSDOM('<!DOCTYPE html><div class="grid"><li>Item 1</li><li>Item 2</li><li>Item 3</li></div>');
+describe('itemsCounter', () => {
+  test('returns the number of li elements', () => {
+    document.body.innerHTML = `
+        <ul class="grid">
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      `;
 
-global.document = dom.window.document;
-// Test case 1
-test('Returns the correct number of li elements in the grid', () => {
-  // Arrange
-  document.body.innerHTML = '<ul class="grid"><li>Item 1</li><li>Item 2</li><li>Item 3</li></ul>';
-  // Act
-  const result = itemsCounter();
-  // Assert
-  expect(result).toBe(3);
-});
-// Test case 2: Check if the function returns 0 when the grid has no li elements
-test('Returns 0 when the grid has no li elements', () => {
-  // Arrange
-  document.body.innerHTML = '<ul class="grid"></ul>';
-  // Act
-  const result = itemsCounter();
-  // Assert
-  expect(result).toBe(0);
-});
-// Test case 3
-test('Returns the correct number of li elements in the grid with 5 li elements', () => {
-  // Arrange
-  document.body.innerHTML = `
-    <ul class="grid">
-      <li>Item 1</li>
-      <li>Item 2</li>
-      <li>Item 3</li>
-      <li>Item 4</li>
-      <li>Item 5</li>
-    </ul>
-  `;
-  // Act
-  const result = itemsCounter();
-  // Assert
-  expect(result).toBe(5);
+    document.body.innerHTML = `
+      <ul class="grid">
+        ${Array.from({ length: 5 }).map(() => '<li></li>')}
+      </ul>
+    `;
+
+    const numberOfElements = itemsCounter();
+
+    expect(numberOfElements).toBe(5);
+  });
 });
