6dc2d0881473f36bf51ccc8445f2f6b61f252d6a
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-08-03 10:40:35 +0300

    Update itemsCounter.js

--- src/modules/itemsCounter.js
+++ src/modules/itemsCounter.js
@@ -1,9 +1,10 @@
-
-export const displayAmountMessage = (count) => {
+const displayAmountMessage = (count) => {
     const message = document.createElement('p');
     message.classList.add('item-count');
     message.textContent = `We have ${count} meals`;
     const foodSection = document.querySelector('.foodSection');
     foodSection.insertBefore(message, foodSection.firstChild);
   };
+  
+  export default displayAmountMessage;
   
